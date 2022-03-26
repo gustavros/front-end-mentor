17ff4b4193c92f433dde436b164fd32a747fbcfa
Gustavo <gustavossw@hotmail.com>
2022-03-26 18:11:10 -0300

    update

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,10 @@
+let errorIcon = document.getElementById('img-error')
+let email = document.getElementById('email').value.length
+
+if (email == '') {
+    alert('Please enter a valid email')
+} else {
+    alert('')
+}
+
+
