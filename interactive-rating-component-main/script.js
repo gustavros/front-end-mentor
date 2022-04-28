f8c17a1bba02e4e714950bcc1403e25e8205f33b
Gustavo Santana <gustavossw@hotmail.com>
2022-04-28 10:08:41 -0300

    resolution++

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,32 +1,34 @@
-const buttons = document.querySelectorAll(".number-container button");
-const buttonsContainer = document.querySelector(".number-container ");
-console.log(buttonsContainer);
-const content = document.querySelector(".text-container");
-const btnSubmit = document.querySelector(".btn-submit button");
+const submit_btn = document.querySelector(".btn-submit"); // get the submit button
+const card_content_1 = document.querySelector(".card-container-1"); // get the first card
+const card_content_2 = document.querySelector(".card-container-2"); // get the second card
+const ratingBtns = document.querySelectorAll(".number-container"); // get the rating buttons
+const resetBtn = document.querySelector(".reset-btn");
+const score = document.querySelector(".score"); // get the score feedback
+let stars_score = 3; // default value
 
-const starContainer = document.querySelector(".stars-container");
-
-const thanks = /*html*/ `
-<div class="thanks">
-<img src="images/illustration-thank-you.svg" alt=""/>
-    <span>Você selecionou 1 de 5</span>
-    <h1>Obrigado!</h1>
-    <p>Nós agradecemos seu feedback. Se caso você precise de mais ajuda, não hesite em nos dar um toque!</p>
-</div>
-
-`;
-
-buttons.forEach((button) => {
-  button.addEventListener("click", function () {
-    let buttonSelected = button.innerHTML;
-    console.log(buttonSelected);
-  });
+submit_btn.addEventListener("click", onSubmit); // add the eventlistener a submit button click
+ratingBtns.forEach((btn) => {
+  // for each rating button
+  btn.addEventListener("click", handleRatingBtnClick); // add the eventlistener
 });
 
-btnSubmit.addEventListener("click", function () {
-  starContainer.remove();
-  buttonsContainer.remove();
-  btnSubmit.remove();
-
-  content.innerHTML = thanks;
+resetBtn.addEventListener("click", () => {
+  card_content_2.remove();
 });
+
+function onSubmit() {
+  // when click submit button
+  card_content_1.classList.add("hide"); // hide the first card
+  score.textContent = stars_score; // show the score
+  card_content_2.classList.remove("hide"); // show the second card
+  //console.log('submit click');
+}
+
+function handleRatingBtnClick(event) {
+  // when click rating button
+  // parse stars score
+  stars_score = event.target.textContent; //!important
+  //console.log(stars_score);
+}
+
+//console.log(ratingBtns);
